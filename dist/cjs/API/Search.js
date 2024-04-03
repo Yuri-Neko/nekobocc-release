e4fb6ca5bf17279375f638d1ad39d7efb083ecd8
Kyouka Hashiba <91820453+Yuri-Neko@users.noreply.github.com>
2024-04-03 14:29:42 +0700

    Update Search.js

--- dist/cjs/API/Search.js
+++ dist/cjs/API/Search.js
@@ -19,12 +19,33 @@ const search = async (query) => {
     const array = [];
     $('div.result div.top').each((_i, e) => {
         const img = $(e).find('div.limitnjg > img').attr('src');
-        const title = $(e).find('h2').text();
+        const title = $(e).find('h2 > a').text();
         const url = $(e).find('h2 > a').attr('href');
+       
+        const desc = $(e).find('.desc').text();
+        const sinopsisElement = $(e).find('.desc p:contains("Sinopsis")');
+        let sinopsis = "Sinopsis tidak tersedia";
+        if (sinopsisElement.length > 0) {
+          sinopsis = sinopsisElement.next('p').text().trim();
+        }
+
+        const matchGenre = desc.match(/Genre :(.*)Anime :/);
+        const genre = matchGenre ? matchGenre[1].trim() : "Genre tidak tersedia";
+
+        const matchDuration = desc.match(/Duration :(.*)Size/);
+        const duration = matchDuration ? matchDuration[1].trim() : "Durasi tidak tersedia";
+
+        const matchProducers = desc.match(/Producers :(.*)Duration/);
+        const producers = matchProducers ? matchProducers[1].trim() : "Produser tidak tersedia";
+        
         array.push({
             img,
             title,
-            url
+            url,
+            sinopsis,
+            genre,
+            producers,
+            duration
         });
     });
     return array;
